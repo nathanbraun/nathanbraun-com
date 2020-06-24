8db411cfdad06f201ef049f820211fdee3187407
Nathan Braun <nathanericbraun@gmail.com>
2020-06-24 11:12:41 -0500

    Refactor renderer out into separate module.

--- src/MarkDownRenderer.elm
+++ src/MarkDownRenderer.elm
@@ -1,4 +1,4 @@
-module MarkdownRenderer exposing (TableOfContents, view)
+module MarkdownRenderer exposing (elmUiRenderer)
 
 import Element exposing (Element)
 import Element.Background
@@ -6,76 +6,49 @@ import Element.Border
 import Element.Font as Font
 import Element.Input
 import Element.Region
-import Html exposing (Attribute, Html)
-import Html.Attributes exposing (property)
-import Json.Encode as Encode exposing (Value)
+import Html exposing (Html)
+import Html.Attributes
 import Markdown.Block as Block exposing (Block, Inline, ListItem(..), Task(..))
 import Markdown.Html
-import Markdown.Parser
 import Markdown.Renderer
-import Palette
-import SyntaxHighlight
 
 
-buildToc : List Block.Block -> TableOfContents
-buildToc blocks =
-    let
-        headings =
-            gatherHeadings blocks
-    in
-    headings
-        |> List.map Tuple.second
-        |> List.map
-            (\styledList ->
-                { anchorId = styledToString styledList |> rawTextToId
-                , name = styledToString styledList
-                , level = 1
-                }
-            )
-
-
-type alias TableOfContents =
-    List { anchorId : String, name : String, level : Int }
-
-
-view : String -> Result String ( TableOfContents, List (Element msg) )
-view markdown =
-    case
-        markdown
-            |> Markdown.Parser.parse
-    of
-        Ok okAst ->
-            case Markdown.Renderer.render renderer okAst of
-                Ok rendered ->
-                    Ok ( buildToc okAst, rendered )
-
-                Err errors ->
-                    Err errors
-
-        Err error ->
-            Err (error |> List.map Markdown.Parser.deadEndToString |> String.join "\n")
-
-
-renderer : Markdown.Renderer.Renderer (Element msg)
-renderer =
+elmUiRenderer : Markdown.Renderer.Renderer (Element msg)
+elmUiRenderer =
     { heading = heading
     , paragraph =
         Element.paragraph
-            [ Element.spacing 15 ]
-    , thematicBreak = Element.none
-    , text = \value -> Element.paragraph [] [ Element.text value ]
-    , strong = \content -> Element.paragraph [ Font.bold ] content
-    , emphasis = \content -> Element.paragraph [ Font.italic ] content
+            [ Element.spacing 8, Element.width Element.fill ]
+    , thematicBreak =
+        Element.row
+            [ Element.width Element.fill
+            , Element.Border.widthEach
+                { top = 0
+                , right = 0
+                , bottom = 3
+                , left = 0
+                }
+            , Element.paddingXY 0 10
+            , Element.Border.color (Element.rgb255 145 145 145)
+            , Element.Background.color (Element.rgb255 245 245 245)
+            ]
+            [ Element.none ]
+    , text = Element.text
+    , strong = Element.row [ Font.bold ]
+    , emphasis = Element.row [ Font.italic ]
     , codeSpan = code
     , link =
         \{ title, destination } body ->
-            Element.newTabLink []
+            Element.link
+                [ Element.htmlAttribute
+                    (Html.Attributes.style "display"
+                        "inline-flex"
+                    )
+                ]
                 { url = destination
                 , label =
                     Element.paragraph
-                        [ Font.color (Element.rgb255 0 0 255)
-                        , Element.htmlAttribute (Html.Attributes.style "overflow-wrap" "break-word")
-                        , Element.htmlAttribute (Html.Attributes.style "word-break" "break-word")
+                        [ Font.color (Element.rgb255 7 81 219)
                         ]
                         body
                 }
@@ -84,14 +57,27 @@ renderer =
         \image ->
             case image.title of
                 Just title ->
-                    Element.image [ Element.width Element.fill ] { src = image.src, description = image.alt }
+                    Element.image [ Element.width Element.fill ]
+                        { src =
+                            image.src
+                        , description = image.alt
+                        }
 
                 Nothing ->
-                    Element.image [ Element.width Element.fill ] { src = image.src, description = image.alt }
+                    Element.image [ Element.width Element.fill ]
+                        { src =
+                            image.src
+                        , description = image.alt
+                        }
     , blockQuote =
         \children ->
             Element.column
-                [ Element.Border.widthEach { top = 0, right = 0, bottom = 0, left = 10 }
+                [ Element.Border.widthEach
+                    { top = 0
+                    , right = 0
+                    , bottom = 0
+                    , left = 10
+                    }
                 , Element.padding 10
                 , Element.Border.color (Element.rgb255 145 145 145)
                 , Element.Background.color (Element.rgb255 245 245 245)
@@ -99,11 +85,15 @@ renderer =
                 children
     , unorderedList =
         \items ->
-            Element.column [ Element.spacing 15 ]
+            Element.paragraph
+                [ Element.spacing 6
+                , Element.paddingXY 5 0
+                , Element.width Element.fill
+                ]
                 (items
                     |> List.map
                         (\(ListItem task children) ->
-                            Element.paragraph [ Element.spacing 5 ]
+                            Element.row [ Element.spacing 5 ]
                                 [ Element.row
                                     [ Element.alignTop ]
                                     ((case task of
@@ -114,10 +104,15 @@ renderer =
                                             Element.Input.defaultCheckbox True
 
                                         NoTask ->
-                                            Element.text "•"
+                                            Element.el
+                                                [ Font.size 24
+                                                , Element.paddingXY 10 0
+                                                , Element.alignTop
+                                                ]
+                                                (Element.text "•")
                                      )
                                         :: Element.text " "
-                                        :: children
+                                        :: [ Element.paragraph [] children ]
                                     )
                                 ]
                         )
@@ -130,11 +125,27 @@ renderer =
                         (\index itemBlocks ->
                             Element.row [ Element.spacing 5 ]
                                 [ Element.row [ Element.alignTop ]
-                                    (Element.text (String.fromInt (index + startingIndex) ++ " ") :: itemBlocks)
+                                    (Element.text
+                                        (String.fromInt
+                                            (index
+                                                + startingIndex
+                                            )
+                                            ++ " "
+                                        )
+                                        :: itemBlocks
+                                    )
                                 ]
                         )
                 )
     , codeBlock = codeBlock
+    , html =
+        Markdown.Html.oneOf
+            [ Markdown.Html.tag "emphasize"
+                (\name children ->
+                    Element.column [ Font.size 56 ] (Element.text name :: children)
+                )
+                |> Markdown.Html.withAttribute "name"
+            ]
     , table = Element.column []
     , tableHeader = Element.column []
     , tableBody = Element.column []
@@ -143,84 +154,49 @@ renderer =
         \maybeAlignment children ->
             Element.paragraph [] children
     , tableCell = Element.paragraph []
-    , html =
-        Markdown.Html.oneOf
-            [ Markdown.Html.tag "banner"
-                (\children ->
-                    Element.paragraph
-                        [ Font.center
-                        , Font.size 47
-                        , Font.family [ Font.typeface "Montserrat" ]
-                        , Font.color Palette.color.primary
-                        ]
-                        children
-                )
-            , Markdown.Html.tag "box"
-                (\children ->
-                    Element.textColumn
-                        [ Element.centerX
-                        , Font.center
-                        , Element.padding 30
-                        , Element.Border.shadow { offset = ( 2, 2 ), size = 3, blur = 3, color = Element.rgba255 40 80 80 0.1 }
-                        , Element.spacing 15
-                        ]
-                        children
-                )
-            , Markdown.Html.tag "values"
-                (\children ->
-                    Element.row
-                        [ Element.spacing 30
-                        , Element.htmlAttribute (Html.Attributes.style "flex-wrap" "wrap")
-                        ]
-                        children
-                )
-            , Markdown.Html.tag "value"
-                (\children ->
-                    Element.column
-                        [ Element.width Element.fill
-                        , Element.padding 20
-                        , Element.spacing 20
-                        , Element.height Element.fill
-                        , Element.centerX
-                        ]
-                        children
-                )
-            ]
     }
 
 
-styledToString : List Inline -> String
-styledToString inlines =
-    --List.map .string list
-    --|> String.join "-"
-    -- TODO do I need to hyphenate?
-    inlines
-        |> Block.extractInlineText
+codeBlock : { body : String, language : Maybe String } -> Element msg
+codeBlock details =
+    Element.el
+        [ Element.Background.color (Element.rgba 0 0 0 0.03)
+        , Element.htmlAttribute (Html.Attributes.style "white-space" "pre")
+        , Element.padding 20
+        , Font.family
+            [ Font.external
+                { url = "https://fonts.googleapis.com/css?family=Source+Code+Pro"
+                , name = "Source Code Pro"
+                }
+            ]
+        ]
+        (Element.text details.body)
 
 
-gatherHeadings : List Block -> List ( Block.HeadingLevel, List Inline )
-gatherHeadings blocks =
-    List.filterMap
-        (\block ->
-            case block of
-                Block.Heading level content ->
-                    Just ( level, content )
-
-                _ ->
-                    Nothing
-        )
-        blocks
-
-
-rawTextToId : String -> String
-rawTextToId rawText =
-    rawText
-        |> String.split " "
-        |> String.join "-"
-        |> String.toLower
+code : String -> Element msg
+code snippet =
+    Element.el
+        [ Element.Background.color
+            (Element.rgba 0 0 0 0.04)
+        , Element.Border.rounded 2
+        , Element.paddingXY 5 3
+        , Font.family
+            [ Font.external
+                { url = "https://fonts.googleapis.com/css?family=Source+Code+Pro"
+                , name = "Source Code Pro"
+                }
+            ]
+        ]
+        (Element.text snippet)
 
 
-heading : { level : Block.HeadingLevel, rawText : String, children : List (Element msg) } -> Element msg
+heading :
+    { level : Block.HeadingLevel
+    , rawText : String
+    , children :
+        List (Element msg)
+    }
+    -> Element msg
 heading { level, rawText, children } =
     Element.paragraph
         [ Font.size
@@ -229,13 +205,14 @@ heading { level, rawText, children } =
                     36
 
                 Block.H2 ->
-                    24
+                    28
 
                 _ ->
                     20
             )
         , Font.bold
-        , Font.family [ Font.typeface "Montserrat" ]
+        , Font.family [ Font.typeface "Roboto" ]
+        , Element.paddingEach { bottom = 0, left = 0, right = 0, top = 15 }
         , Element.Region.heading (Block.headingLevelToInt level)
         , Element.htmlAttribute
             (Html.Attributes.attribute "name" (rawTextToId rawText))
@@ -245,31 +222,8 @@ heading { level, rawText, children } =
         children
 
 
-code : String -> Element msg
-code snippet =
-    Element.el
-        [ Element.Background.color
-            (Element.rgba255 50 50 50 0.07)
-        , Element.Border.rounded 2
-        , Element.paddingXY 5 3
-        , Font.family [ Font.typeface "Roboto Mono", Font.monospace ]
-        ]
-        (Element.text snippet)
-
-
-codeBlock : { body : String, language : Maybe String } -> Element msg
-codeBlock details =
-    SyntaxHighlight.elm details.body
-        |> Result.map (SyntaxHighlight.toBlockHtml (Just 1))
-        |> Result.withDefault
-            (Html.pre [] [ Html.code [] [ Html.text details.body ] ])
-        |> Element.html
-        |> Element.el [ Element.width Element.fill ]
-
-
-editorValue : String -> Attribute msg
-editorValue value =
-    value
-        |> String.trim
-        |> Encode.string
-        |> property "editorValue"
+rawTextToId rawText =
+    rawText
+        |> String.split " "
+        |> String.join "-"
+        |> String.toLower
