bc9bb240406d411249bd45ae72340e26d51ed70e
Nathan Braun <nathanericbraun@gmail.com>
2021-10-18 14:18:24 -0500

    Add DataSource to read files from content/

---
+++ src/Page/Slug_.elm
@@ -0,0 +1,92 @@
+module Page.Slug_ exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import DataSource.Glob as Glob
+import Head
+import Head.Seo as Seo
+import Page exposing (Page, PageWithState, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Pages.Url
+import Shared
+import View exposing (View)
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    { slug : String }
+
+
+page : Page RouteParams Data
+page =
+    Page.prerender
+        { head = head
+        , routes = routes
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+routes : DataSource (List RouteParams)
+routes =
+    content |> DataSource.map (List.map (\x -> RouteParams x.slug))
+
+
+data : RouteParams -> DataSource Data
+data routeParams =
+    DataSource.succeed ()
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = Pages.Url.external "TODO"
+            , alt = "elm-pages logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title" -- metadata.title -- TODO
+        }
+        |> Seo.website
+
+
+type alias Data =
+    ()
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view maybeUrl sharedModel static =
+    View.placeholder static.routeParams.slug
+
+
+type alias Section =
+    { filePath : String
+    , slug : String
+    }
+
+
+content : DataSource (List Section)
+content =
+    Glob.succeed Section
+        |> Glob.captureFilePath
+        |> Glob.match (Glob.literal "content/")
+        |> Glob.capture Glob.wildcard
+        |> Glob.match (Glob.literal ".md")
+        |> Glob.toDataSource
