fd5921d146adb95e079899129c79166b728fa833
Nathan Braun <nathanericbraun@gmail.com>
2021-10-19 21:47:52 -0500

    Render content/index.md for index page

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -1,13 +1,20 @@
 module Page.Index exposing (Data, Model, Msg, page)
 
 import DataSource exposing (DataSource)
+import DataSource.File as StaticFile
+import DataSource.Glob as Glob exposing (Glob)
 import Head
 import Head.Seo as Seo
-import Html.Styled
+import Html.Styled as Html exposing (Html)
+import Markdown.Block exposing (Block)
+import Markdown.Parser
+import Markdown.Renderer
+import OptimizedDecoder exposing (Decoder)
 import Page exposing (Page, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
 import Shared
+import TailwindMarkdownRenderer
 import View exposing (View)
 
 
@@ -34,7 +41,7 @@ page =
 
 data : DataSource Data
 data =
-    DataSource.succeed (Data 1)
+    pageBody
 
 
 head :
@@ -58,7 +65,14 @@ head static =
 
 
 type alias Data =
-    { field1 : Int }
+    { metadata : PageMetadata
+    , body : Shared.Model -> List (Html Msg)
+    }
+
+
+type alias PageMetadata =
+    { title : String
+    }
 
 
 view :
@@ -66,7 +80,77 @@ view :
     -> Shared.Model
     -> StaticPayload Data RouteParams
     -> View Msg
-view maybeUrl sharedModel static =
-    { title = "my page"
-    , body = [ Html.Styled.text "my page html text 2" ]
+view _ sharedModel static =
+    { title = "test title"
+    , body = static.data.body sharedModel
     }
+
+
+pageBody : DataSource Data
+pageBody =
+    Glob.expectUniqueMatch (findBySlug "index")
+        |> DataSource.andThen
+            (withFrontmatter
+                Data
+                frontmatterDecoder
+                renderer2
+            )
+
+
+findBySlug : String -> Glob String
+findBySlug slug =
+    Glob.succeed identity
+        |> Glob.captureFilePath
+        |> Glob.match (Glob.literal "content/")
+        |> Glob.match (Glob.literal slug)
+        |> Glob.match (Glob.literal ".md")
+
+
+renderer2 : List Block -> DataSource (Shared.Model -> List (Html msg))
+renderer2 blocks =
+    blocks
+        |> Markdown.Renderer.render TailwindMarkdownRenderer.renderer
+        |> Result.map
+            (\blockViews model ->
+                blockViews
+                    |> renderAll model
+            )
+        |> DataSource.fromResult
+
+
+renderAll : model -> List (model -> view) -> List view
+renderAll model =
+    List.map ((|>) model)
+
+
+frontmatterDecoder : OptimizedDecoder.Decoder PageMetadata
+frontmatterDecoder =
+    OptimizedDecoder.map PageMetadata
+        (OptimizedDecoder.field "title" OptimizedDecoder.string)
+
+
+withFrontmatter :
+    (frontmatter -> (Shared.Model -> List (Html msg)) -> value)
+    -> Decoder frontmatter
+    -> (List Block -> DataSource (Shared.Model -> List (Html msg)))
+    -> String
+    -> DataSource value
+withFrontmatter constructor frontmatterDecoder2 renderer filePath =
+    DataSource.map2 constructor
+        (StaticFile.onlyFrontmatter
+            frontmatterDecoder2
+            filePath
+        )
+        ((StaticFile.bodyWithoutFrontmatter
+            filePath
+            |> DataSource.andThen
+                (\rawBody ->
+                    rawBody
+                        |> Markdown.Parser.parse
+                        |> Result.mapError (\_ -> "Couldn't parse markdown.")
+                        |> DataSource.fromResult
+                )
+         )
+            |> DataSource.andThen
+                renderer
+        )
