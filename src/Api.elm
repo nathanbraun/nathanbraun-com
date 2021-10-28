02bf890d5ee8edc21104fb8ec264c3bd33ed5f2c
Nathan Braun <nathanericbraun@gmail.com>
2021-10-28 11:44:14 -0500

    Generating RSS feed of titles + date

--- src/Api.elm
+++ src/Api.elm
@@ -3,7 +3,11 @@ module Api exposing (routes)
 import ApiRoute
 import DataSource exposing (DataSource)
 import Html exposing (Html)
+import Pages
+import Post
 import Route exposing (Route)
+import Rss
+import Time
 
 
 routes :
@@ -11,4 +15,66 @@ routes :
     -> (Html Never -> String)
     -> List (ApiRoute.ApiRoute ApiRoute.Response)
 routes getStaticRoutes htmlToString =
-    []
+    [ rss
+        { siteTagline = "tagline"
+        , siteUrl = "https://nathanbraun.com"
+        , title = "elm-pages Blog"
+        , builtAt = Pages.builtAt
+        , indexPage = [ "blog" ]
+        }
+        postsDataSource
+    ]
+
+
+rss :
+    { siteTagline : String
+    , siteUrl : String
+    , title : String
+    , builtAt : Time.Posix
+    , indexPage : List String
+    }
+    -> DataSource.DataSource (List Rss.Item)
+    -> ApiRoute.ApiRoute ApiRoute.Response
+rss options itemsRequest =
+    ApiRoute.succeed
+        (itemsRequest
+            |> DataSource.map
+                (\items ->
+                    { body =
+                        Rss.generate
+                            { title = options.title
+                            , description = options.siteTagline
+                            , url = options.siteUrl ++ "/" ++ String.join "/" options.indexPage
+                            , lastBuildTime = options.builtAt
+                            , generator = Just "elm-pages"
+                            , items = items
+                            , siteUrl = options.siteUrl
+                            }
+                    }
+                )
+        )
+        |> ApiRoute.literal "feed.xml"
+        |> ApiRoute.single
+
+
+postsDataSource : DataSource.DataSource (List Rss.Item)
+postsDataSource =
+    Post.allMetadata
+        |> DataSource.map
+            (List.map
+                (\( route, article ) ->
+                    { title = article.title
+                    , description = article.description
+                    , url =
+                        route
+                            |> Route.routeToPath
+                            |> String.join "/"
+                    , categories = []
+                    , author = "Nathan Braun"
+                    , pubDate = Rss.Date article.published
+                    , content = Nothing
+                    , contentEncoded = Nothing
+                    , enclosure = Nothing
+                    }
+                )
+            )
