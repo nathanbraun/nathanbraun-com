caf3af312724b8cbdabaf518940d9818da62e309
Nathan Braun <nathanericbraun@gmail.com>
2024-07-08 11:14:05 -0500

    feat: add tailwind stuff

---
+++ gen/Tailwind/Breakpoints.elm
@@ -0,0 +1,39 @@
+module Tailwind.Breakpoints exposing (..)
+
+import Css
+import Css.Media
+
+
+lg : List Css.Style -> Css.Style
+lg =
+    Css.Media.withMediaQuery [ "(min-width: 1024px)" ]
+
+
+md : List Css.Style -> Css.Style
+md =
+    Css.Media.withMediaQuery [ "(min-width: 768px)" ]
+
+
+sm : List Css.Style -> Css.Style
+sm =
+    Css.Media.withMediaQuery [ "(min-width: 640px)" ]
+
+
+xl : List Css.Style -> Css.Style
+xl =
+    Css.Media.withMediaQuery [ "(min-width: 1280px)" ]
+
+
+xxl : List Css.Style -> Css.Style
+xxl =
+    Css.Media.withMediaQuery [ "(min-width: 1536px)" ]
+
+
+xxxl : List Css.Style -> Css.Style
+xxxl =
+    Css.Media.withMediaQuery [ "(min-width: 1920px)" ]
+
+
+xxxxl : List Css.Style -> Css.Style
+xxxxl =
+    Css.Media.withMediaQuery [ "(min-width: 2200px)" ]
