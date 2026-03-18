2bb1634c37198c611aeff34711159e819190a8f6
Nathan Braun <nathanericbraun@gmail.com>
2026-03-18 12:58:56 -0600

    fix: trailing slash thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://nathanbraun.com',
+  trailingSlash: 'never',
   integrations: [
     tailwind(),
     sitemap(),
